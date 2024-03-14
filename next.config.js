361ee986a99d3bce7942ec8505d7f4ddef2c4fe7
aleegr10 <94106547+aleegr10@users.noreply.github.com>
2024-03-14 21:31:24 +0100

    Actualizar next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   images: {
-    domains: ['www.smashbros.com', 'images.wikidexcdn.net', 'www.guiasnintendo.com', '2img.net', 'images4.wikia.nocookie.net', 'razageek.com', 'static.vecteezy.com'],
+    domains: [
+      'www.smashbros.com',
+      'images.wikidexcdn.net',
+      'www.guiasnintendo.com',
+      '2img.net',
+      'images4.wikia.nocookie.net',
+      'razageek.com',
+      'static.vecteezy.com'
+    ],
   },
-
+  swcMinify: true, // Añade esta línea para habilitar la minificación con SWC
   webpack: (config, { isServer }) => {
     // Agrega la siguiente línea para activar el modo de depuración de webpack
     config.mode = 'development';
