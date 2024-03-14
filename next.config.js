ef0768313fe670dbbfaa2953411acb951226688b
aleegr10 <tailerby10@gmail.com>
2024-03-14 20:26:16 +0100

    arreglar dependencias

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   images: {
     domains: ['www.smashbros.com', 'images.wikidexcdn.net', 'www.guiasnintendo.com', '2img.net', 'images4.wikia.nocookie.net', 'razageek.com', 'static.vecteezy.com'],
   },
+
+  webpack: (config, { isServer }) => {
+    // Agrega la siguiente línea para activar el modo de depuración de webpack
+    config.mode = 'development';
+
+    return config;
+  },
 }
